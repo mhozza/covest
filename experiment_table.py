20c38156a5fed65227dc3e7f914836d4b00780f4
Michal Hozza <mhozza@gmail.com>
2015-05-08 23:26:46 +0200

    experiment table splited

--- experiment_table.py
+++ experiment_table.py
@@ -4,6 +4,7 @@ import glob
 import os
 import json
 from collections import defaultdict
+from table_generator import *
 
 
 def parse_fname(fname, error=True):
@@ -94,45 +95,6 @@ def parse_estimate(fname):
 def parse_estimate2(fname):
     with open(fname, 'rU') as f:
         return json.load(f)
-
-
-def format_table(header, lines, template, line_template, header_cell_template,
-                 cell_template, round_floats=4):
-    lbody = ''
-    for c in header:
-        lbody += header_cell_template.format(cbody=c)
-    thead = line_template.format(lbody=lbody)
-    tbody = ''
-    for l in lines:
-        lbody = ''
-        for c in header:
-            val = l.get(c, None)
-            if round_floats and type(val) is float:
-                val = round(val, round_floats)
-            lbody += cell_template.format(cbody=val)
-        tbody += line_template.format(lbody=lbody)
-    return template.format(thead=thead, tbody=tbody)
-
-
-def format_table_html(header, lines):
-    template = '''<!DOCTYPE html>
-    <html>
-    <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    </head>
-    <table class="table table-condensed table-hover">
-        {thead}
-        {tbody}
-    </table>
-    </html>
-    '''
-    line_template = '\t<tr>\n{lbody}\t</tr>\n'
-    cell_template = '\t\t<td>{cbody}</td>\n'
-    header_cell_template = '\t\t<th>{cbody}</th>\n'
-    return format_table(header, lines, template, line_template,
-                        header_cell_template, cell_template)
 
 
 def kmer_to_read_coverage(c, k, read_length=100):
