0608446b669cc1a543746f6bf953805544d26853
Michal Hozza <mhozza@gmail.com>
2015-05-15 14:52:00 +0200

    compute coverage from genome size

--- gsest.py
+++ gsest.py
@@ -5,13 +5,17 @@ from covest import count_reads_size
 
 def main(args):
     rc = count_reads_size(args.reads)
-    print('Genome size:', int(round(rc / args.coverage)))
+    if args.coverage:
+        print('Genome size:', int(round(rc / args.coverage)))
+    if args.genome_size:
+        print('Genome size:', int(round(rc / args.genome_size)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Simulate reads form random genome with errors')
     parser.add_argument('reads', help='Input histogram')
-    parser.add_argument('coverage', type=float, help='Coverage')
+    parser.add_argument('-c', '--coverage', type=float, help='Coverage')
+    parser.add_argument('-g', '--genome-size', type=float, help='Genome size')
 
     args = parser.parse_args()
     main(args)
