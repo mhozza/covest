9c8a0b42f2171714cd0dce8fdee5df5dbd00b9fd
Michal Hozza <mhozza@gmail.com>
2016-04-21 17:00:02 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ covest_poisson = Extension(
 
 setup(
     name='covest',
-    version='0.1.0',
+    version='0.1.1',
     description="Covest estimates the coverage and genom size, "
     "just from k-mer abundance histogram computed from DNA sequences reads.",
     long_description=readme,
